3f17ab0188162ab78094708947b9ef9ff252efe1
EthanLim0129 <130417126+EthanLim0129@users.noreply.github.com>
2023-05-04 17:12:07 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c4vs9fqxkgy7.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limlianghui-employee"
+customregion = "us-east-1"
